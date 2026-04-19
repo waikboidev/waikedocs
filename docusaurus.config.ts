5bae9b52b8b0a6c2c24d2e7fcc424ceeb956af25
waikboidev <waikboidev@waike.dev>
2026-04-19 15:54:49 +1000

    ah

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ltdocs.waike.dev',
+  url: 'https://waikboidev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
